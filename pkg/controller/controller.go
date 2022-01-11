d80a511a9916d91c30c2c9c3c6f04c9c05b32537
Matt Steen <msteen@aurorasolar.com>
2022-01-11 12:57:15 -0500

    more view WIP

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -6,6 +6,10 @@ import (
 	"github.com/rivo/tview"
 )
 
+const (
+	descTitleRatio = 2
+)
+
 // Controller mediates between the model and the view.
 type Controller struct {
 	db  *db.Database
@@ -29,8 +33,8 @@ func (c *Controller) Go() {
 	text := c.getHeader("open")
 	table := c.getTable("open")
 
-	grid.AddItem(text, 0, 0, 1, 10, 0, 0, false)
-	grid.AddItem(table, 1, 0, 1, 10, 0, 0, true)
+	grid.AddItem(text, 0, 0, 1, 1, 0, 0, false)
+	grid.AddItem(table, 1, 0, 1, 1, 0, 0, true)
 
 	if err := c.app.SetRoot(grid, true).SetFocus(grid).Run(); err != nil {
 		panic(err)
@@ -38,6 +42,7 @@ func (c *Controller) Go() {
 }
 
 // TODO: is there a way to define the keyboard shortcuts for each status in one place?
+// reuse in the actual handling and to display.
 func (c *Controller) getHeader(status string) *tview.TextView {
 	text := tview.NewTextView()
 	text.SetScrollable(false)
@@ -50,8 +55,23 @@ func (c *Controller) getTable(status string) *tview.Table {
 	table := tview.NewTable().SetBorders(false)
 
 	for row, todo := range c.db.Statuses[status].Todos {
-		table.SetCell(row, 0, tview.NewTableCell(todo.Title).SetExpansion(10))
-		table.SetCell(row, 1, tview.NewTableCell(todo.Description).SetExpansion(20))
+		col := 0
+		table.SetCell(row, col, tview.NewTableCell(todo.Title).SetExpansion(1).SetReference(todo))
+		col++
+
+		table.SetCell(row, col, tview.NewTableCell(todo.Description).SetExpansion(descTitleRatio))
+		col++
+
+		labels := ""
+		for _, l := range todo.Labels {
+			if len(labels) > 0 {
+				labels += ", "
+			}
+
+			labels += l.Name
+		}
+
+		table.SetCell(row, col, tview.NewTableCell(labels).SetTextColor(tcell.ColorGreen).SetExpansion(1))
 	}
 
 	table.SetSelectable(true, false)
@@ -62,6 +82,21 @@ func (c *Controller) getTable(status string) *tview.Table {
 			c.app.Stop()
 		}
 	})
+
+	// progress on adding arbitrary key actions:
+	// I'm still not sure how to find the currently selected row in that context...
+	//
+	// from k9s:
+	//
+	// func (a *App) keyboard(evt *tcell.EventKey) *tcell.EventKey {
+	// 	if k, ok := a.HasAction(ui.AsKey(evt)); ok && !a.Content.IsTopDialog() {
+	// 		return k.Action(evt)
+	// 	}
+	//
+	// 	return evt
+	// }
+	//
+	// c.app.SetInputCapture(c.keyboard)
 
 	// TODO: I think this is where I define shortcut key behavior, etc
 	/* table.SetSelectedFunc(func(row int, column int) {
